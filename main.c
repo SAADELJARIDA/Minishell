2c423903d42175fc8f7ae11f403a531ae20fd330
Saad-Eddine El Jarida <sel-jari@e1r4p9.1337.ma>
2025-06-11 08:53:28 +0100

    tests

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int main()
     while (True)
     {
         input = readline("\033[1;32m➜\033[0m\033[1;36m Minishell@damn:$\033[0m ");
-        parse_input();
+        parse_input(input);
         free(input);
     }
 }
